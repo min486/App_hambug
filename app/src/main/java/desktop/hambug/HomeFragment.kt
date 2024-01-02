0f02e6de4b0925b1ff40de8252925d66a50ddb2b
min486 <smh92333@gmail.com>
2024-01-02 23:02:36 +0900

    feat: infinite & auto 이미지 슬라이드

--- app/src/main/java/desktop/hambug/HomeFragment.kt
+++ app/src/main/java/desktop/hambug/HomeFragment.kt
@@ -1,17 +1,68 @@
 package desktop.hambug
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import androidx.fragment.app.Fragment
-import desktop.hambug.databinding.FragmentAuthBinding
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 import desktop.hambug.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
 
     private lateinit var binding : FragmentHomeBinding
+    private lateinit var viewPager: ViewPager2
+    private lateinit var handler : Handler
+    private lateinit var imageList: ArrayList<Int>
+    private lateinit var adapter: ImageAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeBinding.bind(view)
+
+        init()
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                handler.removeCallbacks(runnable)
+                handler.postDelayed(runnable, 2000)
+            }
+        })
     }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable, 2000)
+    }
+
+    private val runnable = Runnable {
+        viewPager.currentItem = viewPager.currentItem + 1
+    }
+
+    private fun init() {
+        viewPager = binding.imageViewPager
+        handler = Handler(Looper.myLooper()!!)
+        imageList = ArrayList()
+
+        imageList.add(R.drawable.burger)
+        imageList.add(R.drawable.burger2)
+        imageList.add(R.drawable.burger3)
+
+        adapter = ImageAdapter(imageList, viewPager)
+
+        viewPager.adapter = adapter
+        viewPager.clipToPadding = false
+        viewPager.clipChildren = false
+        viewPager.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+    }
+
 }
+
